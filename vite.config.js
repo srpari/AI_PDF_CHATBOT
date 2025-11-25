52524819f59186a5c3ff299502f8d2a867a08531
srpari <pari.ramamoorthy@gmail.com>
2025-11-25 14:09:25 -0500

    vite update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/AI_PDF_CHATBOT/",
+  base: import.meta.env.VITE_BASE_PATH || "/AI_PDF_CHATBOT",
   plugins: [react()],
 })
